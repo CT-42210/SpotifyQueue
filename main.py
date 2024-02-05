6adf033f44437318f5c6380214966e98fdd289f7
Nick T <nicholastroiano123@gmail.com>
2024-02-04 21:09:57 -0500

    now it works better and i fixed small things like the title and icon

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import spotifySetup
 import ngrok
 
 load_dotenv()
@@ -23,8 +24,7 @@ sp_oauth = SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_s
 
 token_info = sp_oauth.get_cached_token()
 if not token_info:
-    print("Please run the script on your server first to get a token")
-    exit()
+    spotifySetup.authenticate(scope, client_id, client_secret, redirect_uri, username)
 
 sp = spotipy.Spotify(auth=token_info['access_token'])
 
@@ -55,6 +55,6 @@ def play(song_uri):
 
 
 if __name__ == '__main__':
-    # port = ngrok.connect(authtoken=ngrok_authtoken, service="http", port="5000")
+    # port = ngrok.connect(authtoken=ngrok_authtoken, service="http", domain="spotifyqueue.ngrok.app", port="5000")
     # print(f"Ingress established at {port.url()}")
     app.run(debug=True)
--- spotifySetup.py
+++ spotifySetup.py
@@ -1,26 +1,14 @@
-import os
-from dotenv import load_dotenv
-import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-load_dotenv()
 
-client_id = os.getenv('ID')
-client_secret = os.getenv('SECRET')
-redirect_uri = os.getenv('REDIRECT')
-username = os.getenv('USERNAME')
-scope = 'user-modify-playback-state user-read-playback-state'
+def authenticate(scope, client_id, client_secret, redirect_uri, username):
+    sp_oauth = SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri,
+                            username=username)
 
-sp_oauth = SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri,
-                        username=username)
-
-token_info = sp_oauth.get_cached_token()
-if not token_info:
-    auth_url = sp_oauth.get_authorize_url()
-    print(f"Please navigate here: {auth_url}")
-    response = input("Enter the URL you were redirected to: ")
-    code = sp_oauth.parse_response_code(response)
-    token_info = sp_oauth.get_access_token(code)
-
-# At this point, the token is cached, and you can run your main application
-print("Token cached, you can now run the main application.")
+    token_info = sp_oauth.get_cached_token()
+    if not token_info:
+        auth_url = sp_oauth.get_authorize_url()
+        print(f"Please navigate here: {auth_url}")
+        response = input("Enter the URL you were redirected to: ")
+        code = sp_oauth.parse_response_code(response)
+        token_info = sp_oauth.get_access_token(code)
